a77a0f31bd955873447b2ab0c7d367c42a0c7ebd
vincentbailey <vincentbailey2002@live.com>
2017-03-02 12:20:15 -0800

    Thursday - your favorite quote

---
+++ week8/1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("assets"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func css(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/css/main.css")
+}
+
+func bali(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/img/pic.jpg")
+}
